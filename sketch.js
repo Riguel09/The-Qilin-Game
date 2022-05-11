bc465f5b56f526eb0f7fffdf0d0507b8adfb26c3
Riguel09 <70649561+Riguel09@users.noreply.github.com>
2022-05-11 10:05:55 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,6 +4,14 @@ let gravity = 2;
 let floorHeight = 100;
 let covid;
 let enemies = [];
+let enemySpeed = 3;
+let score = 0;
+
+function preload() {
+  qilin = loadImage("qilin.png");
+  covid = loadImage("coronavirus-5107804_1280.webp");
+  back = loadImage("background.jpg");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -13,100 +21,100 @@ function setup() {
 function draw() {
   background(back);
   
-  if(random(1) < 0.01){
+  enemySpeed += 0.001; /// ***
+  
+  if (random(1) < 0.03) {
     enemies.push(new Enemy());
   }
-  for(let i of enemies){
+  
+  for (let i of enemies) {
     i.move();
-    i.display()
-    if(p.collision(i)){
-    console.log("Game Over")
-    noLoop()
-  }
+    i.display();
+    if (p.collision(i)) {
+      //console.log("Game Over");
+      //noLoop();
+    }
   }
   p.update();
   p.display();
   
+  textSize(50);
+  fill(255);
+  text("Score: " + score, 35, 70); // play with a different font?
 }
 
-function keyPressed(){
-  if (key == " "){
-    p.jump()
+function keyPressed() {
+  if (key == " ") {
+    p.jump();
   }
 }
-
-function preload(){
-  qilin = loadImage("qilin.png")
-  covid = loadImage("coronavirus-5107804_1280.webp")
-  back = loadImage("background.jpg")
-}
-
 
 class Player {
   constructor() {
     this.position = {
-      x: 50,
+      x: 100,
       y: 90,
-    }; // M: oh this looks great!
-
+    };
     this.velocity = {
       y: 0,
     };
-
     this.width = 125;
     this.height = 125;
   }
   display() {
+    push();
     noStroke();
     fill(color(0, 0, 255));
-    image(qilin,this.position.x, this.position.y, this.width, this.height);
+    image(qilin, this.position.x, this.position.y, this.width, this.height);
+    pop();
   }
-
-  jump(){
-      this.velocity.y = -30
+  jump() {
+    this.velocity.y = -30;
   }
-  
-  collision(i){
-    
-    if(i.x > this.position.x && i.x < this.width + this.position.x && i.y >       this.position.y && i.y < this.position.y + this.height){
-      return true
+  collision(i) {
+    if (
+      i.x > this.position.x &&
+      i.x < this.width + this.position.x &&
+      i.y > this.position.y &&
+      i.y < this.position.y + this.height
+    ) {
+      //return true;
+      // increase the score
+      score++; // ***
+      
+      // shoot the enemy away
+      i.xSpd = random(10, 40);
+      i.ySpd = random(-5, 5);
     }
-    
   }
-
   update() {
     this.position.y += this.velocity.y;
 
-    if (
-      this.position.y + this.height + this.velocity.y <=
-      windowHeight
-    ) {
+    if (this.position.y + this.height + this.velocity.y <= windowHeight) {
       this.velocity.y += gravity;
     } else {
       this.velocity.y = 0;
     }
-
   }
-  
 }
 
-class Enemy{
-  
-  constructor(){
-    this.x = width
-    this.y = windowHeight - 75
-    this.width = 75;
-    this.height = 75;
-    
-    
+class Enemy {
+  constructor() {
+    this.x = width;
+    this.y = random(height);
+    this.xSpd = random(-2, -1) * enemySpeed; // ***
+    this.ySpd = random(-3, 3);
+    this.size = random(30, 75);
   }
-  
-  move(){
-    this.x -= 10
+  move() {
+    this.x += this.xSpd;
+    this.y += this.ySpd;
   }
-
-  display(){
-    image(covid, this.x, this.y, this.width, this.height)
+  display() {
+    push();
+    imageMode(CENTER);
+    image(covid, this.x, this.y, this.size, this.size);
+    pop();
   }
 }
 
